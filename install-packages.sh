51af5371fb5347ecfc005e96067fd4534bb17327
jaska <pusanteemu@gmail.com>
2021-01-31 10:26:10 +0200

    add vim plug

--- install-packages.sh
+++ install-packages.sh
@@ -2,3 +2,6 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo bash
 sudo apt-get install -y nodejs
 
 sudo snap install nvim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
